1f543cfdc506a96f762a81ae961a0846af1fa7b7
Jaider <jaidercueto.1010@gmail.com>
2024-09-11 12:22:46 -0500

    Adding styles. Solving problem when inserting icons in inputs

--- src/app/components/EmojiPicker.jsx
+++ src/app/components/EmojiPicker.jsx
@@ -1,15 +1,48 @@
-import { data as EmojiList } from "../data";
+import { data } from "../data";
+import { useState } from "react";
 
 export function EmojiPicker({ onEmojiClicked }) {
+  const [emojiList, setEmojiList] = useState(data);
+
+  function handleChange(e) {
+    if (e.target.value.trim().length) {
+      const newEmojiList = data.filter((emoji) =>
+        emoji.keywords.includes(e.target.value)
+      );
+      setEmojiList(newEmojiList);
+    } else {
+      setEmojiList(data);
+    }
+  }
+
   return (
-    <div class="w-full">
-      {EmojiList.map((emoji) => {
-        return (
-          <button key={emoji.name} onClick={() => onEmojiClicked(emoji.symbol)}>
-            {emoji.symbol}
-          </button>
-        );
-      })}
+    <div className="w-full p-4 bg-black shadow-lg rounded-tl-lg rounded-bl-lg rounded-br-lg gap-3">
+      {
+        emojiList.length ?
+          <div className="grid grid-cols-6 gap-4 mb-3">
+            {
+              emojiList.map((emoji) => {
+                return (
+                  <button
+                    key={emoji.name}
+                    className="p-2 text-2xl hover:bg-gray-100 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    onClick={() => onEmojiClicked(emoji.symbol)}
+                  >
+                    {emoji.symbol}
+                  </button>
+                );
+              })
+            }
+          </div> :
+          <p className="text-white text-center italic mb-3" >No results</p>
+      }
+      <input
+        className="flex-1 placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded py-1 px-3 shadow-sm"
+        placeholder="Look for emojis..."
+        type="text"
+        name="search"
+        onChange={handleChange}
+      />
     </div>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -18,21 +18,21 @@ export default function Home() {
     const firstSection = inputRef.current.value.slice(0, cursorPosition);
     const lastSection = inputRef.current.value.slice(cursorPosition);
     inputRef.current.value = firstSection + emoji + lastSection;
-    inputRef.current.selectionStart = firstSection.length + 1;
+    inputRef.current.selectionStart = firstSection.length;
     inputRef.current.selectionEnd = inputRef.current.selectionStart;
   }
 
   return (
-    <div class="w-full">
-      <div class="flex w-full">
+    <div className="w-1/2 mx-auto mt-20 min-h-full">
+      <div className="flex w-full">
         <input
           ref={inputRef}
-          class="flex-1 placeholder:italic placeholder:text-slate-400 block bg-white w-1/2 border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+          className="flex-1 placeholder:italic placeholder:text-slate-400 block bg-white w-1/2 border border-slate-300 rounded-tl-lg rounded-bl-lg py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
           placeholder="Write something..."
           type="text"
           name="search"
         />
-        <button class="flex-none" onClick={handleClick}>
+        <button className={`flex-none rounded-tr-lg px-3 ${isDisplayed ? 'bg-black' : ''}` } onClick={handleClick}>
           😄
         </button>
       </div>
